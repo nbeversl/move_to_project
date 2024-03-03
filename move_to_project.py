8bc347c36c889775e9db41c346413590ca6823c1
Nate Beversluis <nathanielbeversluis@gmail.com>
2024-03-03 11:03:27 +0100

    add -leave_links flag; add comments for debugging; replace links in source project manually; return a message of how many nodes were moved

--- move_to_project.py
+++ move_to_project.py
@@ -13,9 +13,10 @@ class MoveNodesToProject:
   name = ['MOVE_TO_PROJECT']
 
   def dynamic_output(self, input_contents):
+
     dest_project = self.project.project_list.get_project_from_link(self.argument_string)
     source_project = self.project
-
+    
     if not dest_project:
       return ''.join([
         input_contents,'\n',
@@ -24,6 +25,7 @@ class MoveNodesToProject:
     moved_nodes = []
     nodes_as_nested = {}
     files_to_move = []
+    leave_links = self.have_flags('-leave_links')
     changed_ids = {} # map old to new (resolved) IDs 
 
     for node_id in list(self.dynamic_definition.included_nodes):
@@ -31,7 +33,10 @@ class MoveNodesToProject:
       nodes_as_nested.setdefault(node.nested, [])
       nodes_as_nested[node.nested].append(node_id)
 
-    for nested_level in nodes_as_nested.keys():
+    # Pop out nodes that are not root level
+    # gather a list of all files that must be moved
+
+    for nested_level in sorted(nodes_as_nested.keys()):
       for node_id in nodes_as_nested[nested_level]:
         if node_id in moved_nodes:
           continue
@@ -41,50 +46,60 @@ class MoveNodesToProject:
           file_to_move = self.project.extensions['POP_NODE']._pop_node(
             node_id,
             from_project=source_project.title(),
-            leave_pointer=False)
+            leave_pointer=False,
+            leave_link=leave_links)
         else:
           file_to_move = self.project.nodes[node_id].filename
         files_to_move.append(file_to_move)
+
+    # move each file, tracking changed IDs once added to the new project
     for file in files_to_move:
-      file_changed_ids = self.project.project_list.move_file(
+      ids_in_new_project = self.project.project_list.move_file(
         file,
         source_project.title(),
         dest_project.title(),
         replace_links=False)
-      if file_changed_ids:
-        changed_ids.update(file_changed_ids)
-    
-    nodes_to_update = self._get_links_to_multiple(moved_nodes)
-    
-    #file update bug here
-    for node_id in nodes_to_update: 
-      for replacement_link in nodes_to_update[node_id]:
-        if replacement_link in changed_ids:
-          resolved_link = changed_ids[replacement_link]
-        else:
-          resolved_link = replacement_link
-        self.project.nodes[node_id].replace_links(
-          replacement_link,
-          new_id=resolved_link,
-          new_project=dest_project.title())
+      if ids_in_new_project:
+        changed_ids.update(ids_in_new_project) 
+        # dict of changed ids received from _check_file_for_duplicates
 
-    return input_contents
+    # update links in both project:
+    # destination project:
+    #   - ? replace the links with resolved ids if they have changed - but this should happen automatically?
+    #   - add the source project to links that still link back to it (are not in the new project).
+
+    for original_id in moved_nodes:
+      dest_project_id = changed_ids[node_id] if node_id in changed_ids else original_id
+      for link in dest_project.nodes[dest_project_id].links:
+        if link.node_id in changed_ids and link.node_id != changed_ids[link.node_id]:
+          if link.node_id not in dest_project.nodes:
+            dest_project.nodes[dest_project_id].replace_links(
+                link.node_id,
+                new_id=link.node_id,
+                new_project=self.project.title())
+      
+    source_proj_links_to_update = self._get_links_to_multiple(moved_nodes)
+    for link_id in source_proj_links_to_update: 
+      for node_with_link_to_update in source_proj_links_to_update[node_id]:
+          if node_with_link_to_update in self.project.nodes:
+            self.project.nodes[node_with_link_to_update].replace_links(
+              link_id,
+              new_id=link_id,
+              new_project=dest_project.title())
+
+    return str(len(moved_nodes)) + ' nodes moved to %s' % self.utils.make_project_link(dest_project.title())
 
   def _get_links_to_multiple(self, to_ids):
       """
-      Give a list of target ids, returns a dict with the keys being
-      ids of project nodes and the values being the target id(s) linked
-      to by that node.
+      Give a list of ids in the current project, returns a dict
+      keys : ids of project nodes
+      values :  id(s) linked to by that node.
       """
       if not isinstance(to_ids, list):
           to_ids = [to_ids]
       links_to = {}
-
-      for project_node in [n for n in self.dynamic_definition.project.nodes.values() if not n.dynamic]:
-        for to_id in to_ids:
-            if to_id in project_node.links_ids():
-                links_to.setdefault(project_node.id,[])
-                links_to[project_node.id].append(to_id)
+      for node_id in to_ids:
+        links_to[node_id] = self.project.get_links_to(node_id, include_dynamic=False)
       return links_to
 
 urtext_directives = [MoveNodesToProject]
